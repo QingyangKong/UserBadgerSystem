f1c71c35d4602df7ac0d17e36f0a7f0b9ae9257e
Kong <qingyangkong@kpmg.com>
2017-09-14 11:44:04 -0600

    add all files

---
+++ UserBadgerSystem.SharedObject/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserBadgerSystem.SharedObject
+{
+    public class Question
+    {
+        public Guid QuestionId { get; set; }
+        public Guid AskedBy { get; set; }
+        public DateTime AskedOn { get; set; }
+        public bool IsOpen { get; set; }
+        public List<Answer> Answers { get; set; }
+        public int Upvotes { get; set; }
+        public int Views { get; set; }
+    }
+}
